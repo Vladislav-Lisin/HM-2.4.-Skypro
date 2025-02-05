762c794f9b14f7027ee24972b5ae7bee12f43945
Vladislav <M1LkMenIO@yandex.ru>
2025-02-05 16:23:53 +0300

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void checkAuthentication(String login, String password, String confirmPassword){
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
