3d21accf1749b3c6da1716a62cd34de5a0820d23
Vladislav-Lisin <151949674+Vladislav-Lisin@users.noreply.github.com>
2025-02-11 18:42:10 +0300

    Revert "correction"

--- src/Main.java
+++ src/Main.java
@@ -26,8 +26,10 @@ public class Main {
             checkAuthentication("java_skypro_go", "D_1hWiKjjP_9", "D_1hWiKjjP_9");
             System.out.println("Аутентификация успешна!");
 
-        } catch (WrongLoginException | WrongPasswordException e) {
+        } catch (WrongLoginException  e) {
             System.err.println("Ошибка аутентификации: " + e.getMessage());
+        } catch (WrongPasswordException e) {
+            System.err.println("Ошибка соотвествия пароля: " + e.getMessage());
         }
     }
 }
